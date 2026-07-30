1e1173d1f862cb90027c0c797132f4128d61754b
Aman Soni <amansoni.pali001@gmail.com>
2026-07-31 03:11:00 +0530

    implement LangChain runnable examples with parallel, passthrough and sequence patterns

---
+++ sequenceRunnable.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_mistralai import ChatMistralAI 
+from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.output_parsers import StrOutputParser 
+
+# prompt = ChatPromptTemplate.from_template(
+#     "Explain {topic} in simple words"
+# )
+
+# model = ChatMistralAI(model = "mistral-small-2506")
+
+# parser = StrOutputParser()
+
+# formatted_prompt = prompt.format_messages(topic="Machine Learning")
+
+# response = model.invoke(formatted_prompt)
+
+# final_output = parser.parse(response.content)
+
+# print(final_output)
+
+
+prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in simple words"
+)
+
+model = ChatMistralAI(model="mistral-small-2506")
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke("Machine Learning")
+print(result)
